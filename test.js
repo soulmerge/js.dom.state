65dfe636077499f902272eaa4cd7de05134a0073
Necdet Can Ateşman <can@strg.at>
2016-09-27 13:06:15 +0200

    Added some unit tests

--- test.js
+++ test.js
@@ -10,23 +10,192 @@ describe('score.dom', function() {
 
         it('should add the score.dom.theater object', function(done) {
             loadScore(['oop', 'dom'], function(score) {
-                expect(score).to.be.an('object');
-                expect(score.dom).to.be.a('function');
-                expect(score.dom.theater).to.be(undefined);
-                loadScore(['oop', 'dom', 'dom.theater'], function(score) {
+                try {
                     expect(score).to.be.an('object');
                     expect(score.dom).to.be.a('function');
-                    expect(score.dom.theater).to.be.an('object');
-                    done();
-                });
+                    expect(score.dom.theater).to.be(undefined);
+                    loadScore(['oop', 'dom', 'dom.theater'], function(score) {
+                        try {
+                            expect(score).to.be.an('object');
+                            expect(score.dom).to.be.a('function');
+                            expect(score.dom.theater).to.be.an('object');
+                            done();
+                        } catch (e) {
+                            done(e);
+                        }
+                    });
+                } catch (e) {
+                    done(e);
+                }
             });
         });
 
         it('should provide the "Stage" and "Scene" classes', function(done) {
             loadScore(['oop', 'dom', 'dom.theater'], function(score) {
-                expect(score.dom.theater.Stage).to.be.a('function');
-                expect(score.dom.theater.Scene).to.be.a('function');
-                done();
+                try {
+                    expect(score.dom.theater.Stage).to.be.a('function');
+                    expect(score.dom.theater.Scene).to.be.a('function');
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            });
+        });
+
+    });
+
+    describe('Stage constructor', function() {
+
+        before(function() {
+            var fixture = document.getElementById('fixture');
+            fixture.innerHTML = 
+                '<div class="ficture_stage"> <!-- the stage -->' +
+                '    <div class="fixture_scene"> <!-- a scene -->' +
+                '    </div>' +
+                '    <div class="fixture_scene"> <!-- another scene -->' +
+                '    </div>' +
+                '</div>';
+        });
+
+        after(function() {
+            var fixture = document.getElementById('fixture');
+            while (fixture.children.length) {
+                fixture.removeChild(fixture.children[0]);
+            }
+        });
+
+        it('should set css classes', function() {
+            loadScore(['oop', 'dom', 'dom.theater'], function(score) {
+                try {
+                    var node = score.dom('#fixture .fixture-stage');
+                    expect(node.hasClass('score-theater-stage')).to.be(false);
+                    var stage = score.dom.theater.Stage(node);
+                    expect(node.hasClass('score-theater-stage')).to.be(true);
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            });
+        });
+
+        it('should provide a node member', function() {
+            loadScore(['oop', 'dom', 'dom.theater'], function(score) {
+                try {
+                    var node = score.dom('#fixture .fixture-stage');
+                    var stage = score.dom.theater.Stage(node);
+                    expect(stage.node).to.be(node);
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            });
+        });
+
+        it('should not change the node content', function() {
+            loadScore(['oop', 'dom', 'dom.theater'], function(score) {
+                try {
+                    var node = score.dom('#fixture .fixture-stage');
+                    var prevHTML = node[0].innerHTML;
+                    var stage = score.dom.theater.Stage(node);
+                    expect(node[0].innerHTML).to.be(prevHTML);
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            });
+        });
+
+    });
+
+    describe('Scene constructor', function() {
+
+        before(function() {
+            var fixture = document.getElementById('fixture');
+            fixture.innerHTML = 
+                '<div class="ficture-stage"> <!-- the stage -->' +
+                '    <div class="fixture-scene fixture-scene-1"> <!-- a scene -->' +
+                '    </div>' +
+                '    <div class="fixture-scene"> <!-- another scene -->' +
+                '    </div>' +
+                '</div>';
+        });
+
+        after(function() {
+            var fixture = document.getElementById('fixture');
+            while (fixture.children.length) {
+                fixture.removeChild(fixture.children[0]);
+            }
+        });
+
+        it('should set css classes', function() {
+            loadScore(['oop', 'dom', 'dom.theater'], function(score) {
+                try {
+                    var stageNode = score.dom('#fixture .fixture-stage');
+                    var scene1Node = score.dom('#fixture .fixture-scene').first;
+                    var scene2Node = score.dom('#fixture .fixture-scene').eq(1);
+                    expect(scene1Node.hasClass('score-theater-scene')).to.be(false);
+                    expect(scene2Node.hasClass('score-theater-scene')).to.be(false);
+                    var stage = score.dom.theater.Stage(stageNode);
+                    expect(scene1Node.hasClass('score-theater-scene')).to.be(false);
+                    expect(scene2Node.hasClass('score-theater-scene')).to.be(false);
+                    var scene1 = score.dom.theater.Scene(stage, 'scene1', scene1Node);
+                    expect(scene1Node.hasClass('score-theater-scene')).to.be(true);
+                    expect(scene2Node.hasClass('score-theater-scene')).to.be(false);
+                    var scene2 = score.dom.theater.Scene(stage, 'scene2', scene2Node);
+                    expect(scene1Node.hasClass('score-theater-scene')).to.be(true);
+                    expect(scene2Node.hasClass('score-theater-scene')).to.be(true);
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            });
+        });
+
+        it('should provide a node member', function() {
+            loadScore(['oop', 'dom', 'dom.theater'], function(score) {
+                try {
+                    var stageNode = score.dom('#fixture .fixture-stage');
+                    var scene1Node = score.dom('#fixture .fixture-scene').first;
+                    var scene2Node = score.dom('#fixture .fixture-scene').eq(1);
+                    var scene1 = score.dom.theater.Scene(stage, 'scene1', scene1Node);
+                    var scene2 = score.dom.theater.Scene(stage, 'scene2', scene2Node);
+                    expect(scene1.node).to.be(scene1Node);
+                    expect(scene2.node).to.be(scene2Node);
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            });
+        });
+
+        it('should refuse duplicate scene names', function() {
+            loadScore(['oop', 'dom', 'dom.theater'], function(score) {
+                try {
+                    var stageNode = score.dom('#fixture .fixture-stage');
+                    var scene1Node = score.dom('#fixture .fixture-scene').first;
+                    var scene2Node = score.dom('#fixture .fixture-scene').eq(1);
+                    var scene1 = score.dom.theater.Scene(stage, 'SAMENAME', scene1Node);
+                    expect(function() { var scene2 = score.dom.theater.Scene(stage, 'SAMENAME', scene2Node); }).to.throwError();
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            });
+        });
+
+        it('should not change the node content', function() {
+            loadScore(['oop', 'dom', 'dom.theater'], function(score) {
+                try {
+                    var stageNode = score.dom('#fixture .fixture-stage');
+                    var scene1Node = score.dom('#fixture .fixture-scene').first;
+                    var prevHTML = scene1Node[0].innerHTML;
+                    var scene1 = score.dom.theater.Scene(stage, 'scene1', scene1Node);
+                    var stage = score.dom.theater.Stage(node);
+                    expect(scene1Node[0].innerHTML).to.be(prevHTML);
+                    done();
+                } catch (e) {
+                    done(e);
+                }
             });
         });
 
