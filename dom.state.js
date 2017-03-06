257c01178c09875ed97ffbfa330c69ecd0a96835
Necdet Can Ateşman <c.atesman@gmx.net>
2017-03-06 16:15:16 +0100

    Change: Remove unconditional score.init dependency
    The module now only requires score.init if there is no other loader
    present. We really don't need score.init if there already is requirejs,
    for example.

--- dom.state.js
+++ dom.state.js
@@ -32,193 +32,191 @@
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
-        define(['bluebird', 'score.init', 'score.dom', 'score.oop'], factory);
+        define(['bluebird', 'score.dom', 'score.oop'], factory);
     } else if (typeof module === 'object' && module.exports) {
         // Node. Does not work with strict CommonJS, but
         // only CommonJS-like environments that support module.exports,
         // like Node.
-        factory(require('bluebird'), require('score.init'), require('score.dom'), require('score.oop'));
+        module.exports = factory(require('bluebird'), require('score.dom'), require('score.oop'));
     } else {
         // Browser globals (root is window)
-        factory(Promise, root.score);
+        root.score.extend('dom.state', ['dom', 'oop'], function(dom, oop) {
+            return factory(Promise, dom, oop);
+        });
     }
-})(this, function(Promise, score) {
+})(this, function(Promise, dom, oop) {
 
-    score.extend('dom.state', ['dom', 'oop'], function() {
+    var Group = oop.Class({
+        __name__: 'StateGroup',
 
-        var Group = score.oop.Class({
-            __name__: 'StateGroup',
+        __init__: function(self, node) {
+            self.node = dom(node);
+            self.node.addClass('score-state-group');
+            self.activeState = null;
+            self.states = {};
+        },
 
-            __init__: function(self, node) {
-                self.node = score.dom(node);
-                self.node.addClass('score-state-group');
-                self.activeState = null;
-                self.states = {};
-            },
+        defineState: function(self, name, node) {
+            return new State(self, name, node);
+        },
 
-            defineState: function(self, name, node) {
-                return new State(self, name, node);
-            },
+        show: function(self, state) {
+            if (typeof state === 'string') {
+                if (typeof self.states[state] === 'undefined') {
+                    throw new Error('State ' + state + ' does not exist!');
+                }
+                state = self.states[state];
+            } else if (!(state instanceof State)) {
+                throw new Error('Argument must be a state, or the name of a state!');
+            }
+            return self._activate(state);
+        },
 
-            show: function(self, state) {
-                if (typeof state === 'string') {
-                    if (typeof self.states[state] === 'undefined') {
-                        throw new Error('State ' + state + ' does not exist!');
-                    }
-                    state = self.states[state];
-                } else if (!(state instanceof State)) {
-                    throw new Error('Argument must be a state, or the name of a state!');
+        _activate: function(self, state) {
+            if (self.transitionPromise) {
+                if (self.transitionPromise.state === state) {
+                    return self.transitionPromise;
                 }
-                return self._activate(state);
-            },
-
-            _activate: function(self, state) {
-                if (self.transitionPromise) {
-                    if (self.transitionPromise.state === state) {
-                        return self.transitionPromise;
-                    }
-                    return self.transitionPromise.then(function() {
-                        self._activate(state);
-                    });
-                }
-                if (state === self.activeState) {
-                    return Promise.resolve();
-                }
-                var promise = Promise.resolve();
-                if (typeof promise.cancellable === 'function') {
-                    promise = promise.cancellable();
-                }
-                if (!state.initialized) {
-                    promise = promise.then(function() {
-                        return state._init();
-                    }).then(function() {
-                        state.trigger('init');
-                        state.initialized = true;
-                    });
-                }
-                if (self.activeState !== null) {
-                    promise = promise.then(function() {
-                        if (!self.activeState.trigger('deactivate')) {
-                            throw new Promise.CancellationError();
-                        }
-                        return self.activeState._deactivate();
-                    }).then(function() {
-                        if (self.activeState.node) {
-                            self.activeState.node.removeClass('score-state--active');
-                        }
-                        if (self.node) {
-                            self.node.removeClass('score-state-group--' + self.activeState.name);
-                        }
-                        self.activeState = null;
-                    });
-                }
-                self.transitionPromise = promise.then(function() {
-                    self.activeState = state;
-                    if (self.node) {
-                        self.node.addClass('score-state-group--' + state.name);
-                    }
-                    if (state.node) {
-                        state.node.addClass('score-state--active');
-                    }
-                    return state._activate();
-                }).then(function() {
-                    state.trigger('activate');
-                    self.transitionPromise = null;
+                return self.transitionPromise.then(function() {
+                    self._activate(state);
                 });
-                self.transitionPromise.state = state;
-                return self.transitionPromise;
-            },
-
-            _register: function(self, state) {
-                if (state.name in self.states) {
-                    throw new Error('State with the name ' + state.name + ' already registered!');
-                }
-                self.states[state.name] = state;
             }
-
-        });
-
-        var State = score.oop.Class({
-            __name__: 'State',
-            __events__: ['init', 'activate', 'deactivate'],
-
-            __init__: function(self, group, name, node) {
-                if (!(group instanceof Group)) {
-                    throw new Error('First argument must be a state.Group object!');
-                }
-                if (typeof name !== 'string') {
-                    throw new Error('Name not a string!');
-                }
-                if (node) {
-                    self.node = score.dom(node);
-                    self.node.addClass('score-state');
-                }
-                self.group = group;
-                self.name = name;
-                self.initialized = false;
-                self.group._register(self);
-            },
-
-            show: function(self) {
-                return self.group._activate(self);
-            },
-
-            _init: function() {
-            },
-
-            _activate: function() {
-            },
-
-            _deactivate: function() {
+            if (state === self.activeState) {
+                return Promise.resolve();
             }
-
-        });
-
-        var ActivatedGroup = score.oop.Class({
-            __name__: 'ActivatedStateGroup',
-            __parent__: Group,
-            __events__: ['activate', 'deactivate'],
-
-            __init__: function(self, node) {
-                self.__super__(node);
-                new InactiveState(self);
-                self.deactivate();
-            },
-
-            deactivate: function(self) {
-                return self.show('inactive');
+            var promise = Promise.resolve();
+            if (typeof promise.cancellable === 'function') {
+                promise = promise.cancellable();
             }
-
-        });
-
-        var InactiveState = score.oop.Class({
-            __name__: 'InactiveState',
-            __parent__: State,
-
-            __init__: function(self, group) {
-                self.__super__(group, 'inactive');
-            },
-
-            _activate: function(self) {
-                self.group.trigger('deactivate');
-            },
-
-            _deactivate: function(self) {
-                self.group.trigger('activate');
+            if (!state.initialized) {
+                promise = promise.then(function() {
+                    return state._init();
+                }).then(function() {
+                    state.trigger('init');
+                    state.initialized = true;
+                });
             }
+            if (self.activeState !== null) {
+                promise = promise.then(function() {
+                    if (!self.activeState.trigger('deactivate')) {
+                        throw new Promise.CancellationError();
+                    }
+                    return self.activeState._deactivate();
+                }).then(function() {
+                    if (self.activeState.node) {
+                        self.activeState.node.removeClass('score-state--active');
+                    }
+                    if (self.node) {
+                        self.node.removeClass('score-state-group--' + self.activeState.name);
+                    }
+                    self.activeState = null;
+                });
+            }
+            self.transitionPromise = promise.then(function() {
+                self.activeState = state;
+                if (self.node) {
+                    self.node.addClass('score-state-group--' + state.name);
+                }
+                if (state.node) {
+                    state.node.addClass('score-state--active');
+                }
+                return state._activate();
+            }).then(function() {
+                state.trigger('activate');
+                self.transitionPromise = null;
+            });
+            self.transitionPromise.state = state;
+            return self.transitionPromise;
+        },
 
-        });
-
-        return {
-
-            Group: Group,
-
-            State: State,
-
-            ActivatedGroup: ActivatedGroup
-
-        };
+        _register: function(self, state) {
+            if (state.name in self.states) {
+                throw new Error('State with the name ' + state.name + ' already registered!');
+            }
+            self.states[state.name] = state;
+        }
 
     });
+
+    var State = oop.Class({
+        __name__: 'State',
+        __events__: ['init', 'activate', 'deactivate'],
+
+        __init__: function(self, group, name, node) {
+            if (!(group instanceof Group)) {
+                throw new Error('First argument must be a state.Group object!');
+            }
+            if (typeof name !== 'string') {
+                throw new Error('Name not a string!');
+            }
+            if (node) {
+                self.node = dom(node);
+                self.node.addClass('score-state');
+            }
+            self.group = group;
+            self.name = name;
+            self.initialized = false;
+            self.group._register(self);
+        },
+
+        show: function(self) {
+            return self.group._activate(self);
+        },
+
+        _init: function() {
+        },
+
+        _activate: function() {
+        },
+
+        _deactivate: function() {
+        }
+
+    });
+
+    var ActivatedGroup = oop.Class({
+        __name__: 'ActivatedStateGroup',
+        __parent__: Group,
+        __events__: ['activate', 'deactivate'],
+
+        __init__: function(self, node) {
+            self.__super__(node);
+            new InactiveState(self);
+            self.deactivate();
+        },
+
+        deactivate: function(self) {
+            return self.show('inactive');
+        }
+
+    });
+
+    var InactiveState = oop.Class({
+        __name__: 'InactiveState',
+        __parent__: State,
+
+        __init__: function(self, group) {
+            self.__super__(group, 'inactive');
+        },
+
+        _activate: function(self) {
+            self.group.trigger('deactivate');
+        },
+
+        _deactivate: function(self) {
+            self.group.trigger('activate');
+        }
+
+    });
+
+    return {
+
+        Group: Group,
+
+        State: State,
+
+        ActivatedGroup: ActivatedGroup
+
+    };
 
 });
